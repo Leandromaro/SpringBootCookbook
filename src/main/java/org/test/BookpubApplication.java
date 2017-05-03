8c21c4b9b4bc8cef7d8526879b95b18ef4f4dc95
leandromaro <leandro.maro@globant.com>
2017-05-02 22:38:30 -0300

    test

--- src/main/java/org/test/BookpubApplication.java
+++ src/main/java/org/test/BookpubApplication.java
@@ -3,14 +3,16 @@ package org.test;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.test.bookpub.StartupRunner;
 
 @SpringBootApplication
+@EnableScheduling
 public class BookpubApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookpubApplication.class, args);
-
 	}
 
 	@Bean
